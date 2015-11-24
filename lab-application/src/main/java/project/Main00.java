0a2d4f57ebae362e1ddd7f515d96382f12780283
Leroux <alexis.leroux8@gmail.com>
2015-11-24 20:55:19 +0100

    Mise à jour du POM de "Application" pour communiquer avec le Server

--- lab-application/src/main/java/project/Main00.java
+++ lab-application/src/main/java/project/Main00.java
@@ -1,5 +1,16 @@
 package project;
 
-public class Main00 {
+import launcher.Launcher;
 
+public class Main00 {
+	final static String CONTEXT = "";
+
+	private void execute() {
+		Launcher.startTomcat(CONTEXT);
+	}
+
+	public static void main(String[] args) throws ClassNotFoundException {
+		Class.forName("org.h2.Driver");
+		new Main00().execute();
+	}
 }
